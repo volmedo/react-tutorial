f54d82a681e41e1ec931efa06a7d95f7958c4ce8
Vic <volmedo@visiotechsecurity.com>
2019-03-25 10:23:31 +0100

    Implement turns

--- src/index.js
+++ src/index.js
@@ -3,26 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-    return (
+  return (
     <button className="square" onClick={props.onClick}>
       {props.value}
-      </button>
-    );
-  }
+    </button>
+  );
+}
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (squares[i] == null) {
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   }
 
@@ -36,7 +40,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
